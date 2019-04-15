c6f4057c149891f60f73bd7a4924aa92787a2424
Marieth <mperezzevallos@gmail.com>
2019-04-15 18:36:48 -0500

    Add logging functionalities

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
   namespace :admin do
-    root 'home#index'
+    
     resources :movies
     resources :series
     resources :rentals
